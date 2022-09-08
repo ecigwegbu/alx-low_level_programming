5d77a5f85f5454faed83349aa4887a55809347c0
ecigwegbu <igwegbu@gmail.com>
2022-09-08 13:34:09 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * This program prints a simple message
+ * and then exits
+ *
+ * Return: 0 - exit status zero if all ok
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
