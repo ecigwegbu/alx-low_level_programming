e914bf8250df6d94fda6962cb24d52ccbb7eac76
ecigwegbu <igwegbu@gmail.com>
2022-09-08 16:28:39 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print a simple message to stderr and exit
+ *
+ * Return: 1 - exit status zrequired 
+ */
+int main(void)
+{
+	system("echo \"and that piece of art is useful\\\" - Dora Korpar, 2015-10-19\" >&2");
+	return (1);
+}
