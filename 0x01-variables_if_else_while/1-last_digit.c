86aab53878432323b0e3410527e622dd98f6cf7a
ecigwegbu <igwegbu@gmail.com>
2022-09-09 09:03:14 -0700

    1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,14 +12,12 @@
 int main(void)
 {
 	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	/* your code goes there */
 	char buff[20];
 	int strSize;
 	char lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	strSize = sprintf(buff, "%d", n);
 	lastDigit = buff[strSize - 1];
