5fcef593fd2f9f86982d03da09b16298bb4d2ed5
ecigwegbu <igwegbu@gmail.com>
2022-09-13 04:50:04 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar.c"
+
+/**
+ * main - prints stuff without the std library
+ * users _putchar.c include file
+ * Return: 0 - exit status zero if all ok
+ */
+int main(void)
+{
+	int i;
+	char ch[] = "_putchar\n";
+
+	for (i = 0; i < 9; i++)
+		_putchar(ch[i]);
+	return (0);
+}
+
