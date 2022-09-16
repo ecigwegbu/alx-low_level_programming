fa56a74bc4dbcf56afbe9f3775285a4ce60af4cc
ecigwegbu <igwegbu@gmail.com>
2022-09-15 20:11:54 -0700

    4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,16 @@
+/*****************************************************************************/
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print_most_numbers
+ * using _putchar()
+ *
+ * Return: 0 if all ok
+ */
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
+
