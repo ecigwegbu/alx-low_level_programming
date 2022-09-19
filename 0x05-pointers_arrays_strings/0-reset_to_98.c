413c0820a043d9d71db0a83a211b9ba6027bf221
ecigwegbu <igwegbu@gmail.com>
2022-09-19 03:23:38 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/*****************************************************************************/
+#include "main.h"
+/**
+ * reset_to_98 - uses pointer to reset a number to 98
+ * uses _putchar()
+ *
+ * @n: the pointer to the number to reset
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _ECI_MAIN_H
+#define _ECI_MAIN_H
+
+/* Return: char printed */
+/* @c: input char */
+int _putchar(char c);
+
+/* @n: integer parameter pointer that will be updated */
+void reset_to_98(int *n);
+#endif
+
