46d79a5d0d8b91e24ea5472bf4e6947341621903
ecigwegbu <igwegbu@gmail.com>
2022-09-20 17:11:30 -0700

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+/*****************************************************************************/
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that generates random 101-crackme passwords
+ *
+ * Return: 0 if all ok
+ */
+int main(void)
+{
+	/* template ... initiate pseudocode */
+
+	return (0);
+}
+
