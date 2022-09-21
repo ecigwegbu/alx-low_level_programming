fbdf1c3b0f1d95a0b268240e8a534beb4e2d2a33
ecigwegbu <igwegbu@gmail.com>
2022-09-21 15:19:38 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef _ECI_MAIN_H
+#define _ECI_MAIN_H
+
+/* @dest: destination buffer */
+/* @src: source string */
+char *_strcat(char *dest, char *src);
+
+/**
+ * _strlen - determines the length of a string
+ * @str: the string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str);
+
+#endif
+
