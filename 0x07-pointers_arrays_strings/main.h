278ea46276dcfe4277982210736c025be412bcbb
ecigwegbu <igwegbu@gmail.com>
2022-09-26 14:12:01 +0100

    0. memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef _ECI_MAIN_H
+#define _ECI_MAIN_H
+
+/* @s: the buffer  */
+/* @b: the char to copy  */
+/* @n: the number of characters  */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
+
