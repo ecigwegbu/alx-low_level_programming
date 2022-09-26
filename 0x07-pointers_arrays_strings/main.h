7d0266dc51be4a44c6516e2879d397fd50ca9848
ecigwegbu <igwegbu@gmail.com>
2022-09-26 20:39:50 +0100

    1. memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,10 @@
 /* @b: the char to copy  */
 /* @n: the number of characters  */
 char *_memset(char *s, char b, unsigned int n);
-
+/* @dest: the dest buffer  */
+/* @src: the src to copy  */
+/* @n: the number of characters to copy */
+/* Return: the new dest */
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
 
