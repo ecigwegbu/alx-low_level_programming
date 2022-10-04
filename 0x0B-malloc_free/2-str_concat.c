7bd4c26d37fe22e2c1f07e7a76183e5d49a46d9a
ecigwegbu <igwegbu@gmail.com>
2022-10-04 20:08:20 +0100

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,25 +3,59 @@
 #include "main.h"
 #include "2-strlen.c"
 #include "0-strcat.c"
+#include "9-strcpy.c"
 /**
  * str_concat - returns the length of a string
  *
- * @s: the string
- * Return: the length of the string
+ * @s1: the base string
+ * @s2: the concat string
+ * Return: the string result
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
 
-	str = (char *) malloc(_strlen(s1) + _strlen(s2) + 1);
-	if (!str)
+	if (s1 && !s2)
 	{
-		return (NULL);
+		str = (char *) malloc(_strlen(s1) + 1);
+		if (!str)
+			return (NULL);
+		else
+			return (_strcpy(str, s1));
+	}
+	else if (!s1 && s2)
+	{
+		str = (char *) malloc(_strlen(s2) + 1);
+		if (!str)
+			return (NULL);
+		else
+			return (_strcpy(str, s2));
+	}
+	else if (!s1 && !s2)
+	{
+		str = (char *) malloc(1);
+		if (!str)
+		{
+			return (NULL);
+		}
+		else
+		{
+			str[0] = '\0';
+			return (str);
+		}
 	}
 	else
 	{
-		_strcat(str, s1);
-		_strcat(str, s2);
+		str = (char *) malloc(_strlen(s1) + _strlen(s2) + 1);
+		if (!str)
+		{
+			return (NULL);
+		}
+		else
+		{
+			_strcat(str, s1);
+			_strcat(str, s2);
+		}
 	}
 	return (str);
 }
