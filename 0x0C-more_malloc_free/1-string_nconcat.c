97ba054e963f091520ff0d684f2e11d8803235f2
ecigwegbu <igwegbu@gmail.com>
2022-10-06 21:32:31 +0100

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "main.h"
 #include "_strlen.c"
-#include "_strcpy.c"
 #include "_strncat.c"
 #include "_strcat.c"
 
@@ -18,59 +17,22 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
+	unsigned int l2;
 
-	_set_n(s2, &n);
-	if (!s1 && s2)    /* only s1 empty */
-	{
-		str = (char *) malloc(_strlen(s2) + 1);
-		if (!str)
-			return (NULL);
-		str[0] = '\0';
-		return (_strncat(str, s2, n));
-	}
-	else if (s1 && !s2)    /* only s2 empty */
-	{
-		str = (char *) malloc(_strlen(s1) + 1);
-		if (!str)
-			return (NULL);
-		str[0] = '\0';
-		return (_strcat(str, s1));
-	}
-	else if (!s1 && !s2)    /* both s1 & s2 empty */
-	{
-		str = (char *) malloc(1);
-		if (!str)
-			return (NULL);
-		str[0] = '\0';
-		return (str);
-	}
-	else if (s1 && s2)    /* neither s1 nor s2 empty */
-	{
-		str = (char *) malloc(_strlen(s1) + _strlen(s2) + 1);
-		if (!str)
-			return (NULL);
-		str[0] = '\0';
-		_strcat(str, s1);
-		_strncat(str, s2, n);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	l2 = (unsigned int) _strlen(s2);
+	n = (n > l2) ? l2 : n;
+
+	str = (char *) malloc(_strlen(s1) + _strlen(s2) + 1);
+	if (!str)
+	return (NULL);
+	str[0] = '\0';
+	_strcat(str, s1);
+	_strncat(str, s2, n);
 	return (str);
-}
-
-
-
-/**
- * _set_n - sets the number of chars to concatenate from s2
- *
- * @s2: the concat string
- * @n: pointer to the number of characters to concat
- */
-void _set_n(char *s2, unsigned int *n)
-{
-	if (s2)
-	{
-		unsigned int l2 = _strlen(s2);
-
-		*n = (*n > l2) ? l2 : *n;
-	}
 }
 
