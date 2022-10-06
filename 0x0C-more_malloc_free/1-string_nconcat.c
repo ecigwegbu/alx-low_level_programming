502b2ccee38a301da03205126530fcdf5bfd964e
ecigwegbu <igwegbu@gmail.com>
2022-10-06 22:43:47 +0100

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,9 +1,10 @@
 /*****************************************************************************/
 #include <stdlib.h>
 #include "main.h"
-#include "_strlen.c"
-#include "_strncat.c"
-#include "_strcpy.c"
+char *_strncatu(char *dest, char *src, unsigned int n);
+unsigned int _strlenu(char *s);
+char *_strcpyu(char *dest, char *src);
+
 
 /**
  * string_nconcat - concatenates n chars from string s2 to s1
@@ -24,17 +25,78 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	l2 = (unsigned int) _strlen(s2);
+	l2 = (unsigned int) _strlenu(s2);
 	n = (n > l2) ? l2 : n;
 
-	str = (char *) malloc(_strlen(s1) + _strlen(s2) + 1);
+	str = (char *) malloc(_strlenu(s1) + _strlenu(s2) + 1);
 	if (!str)
 		return (NULL);
 	str[0] = '\0';
-	_strcpy(str, s1);
-	_strncat(str, s2, n);
-	str[_strlen(s1) + _strlen(s2)] = '\0';
+	_strcpyu(str, s1);
+	_strncatu(str, s2, n);
+	str[_strlenu(s1) + _strlenu(s2)] = '\0';
 
 	return (str);
+}
+
+/**
+ * _strncatu - concatenates n characters from src to dest strings
+ *
+ * @dest: the destination string
+ * @src: the src string
+ * @n: the number of characters to consider
+ *
+ * Return: the resulting string
+ */
+char *_strncatu(char *dest, char *src, unsigned int n)
+{
+	 unsigned int i = 0, dest_sz;
+
+	dest_sz = _strlenu(dest);
+	/* src_sz = _strlen(src); */
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_sz + i] = src[i];
+	dest[dest_sz + i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strlenu - returns the length of a string
+ * uses _putchar()
+ *
+ * @s: the string
+ * Return: the length of the string
+ */
+unsigned int _strlenu(char *s)
+{
+	unsigned int i = 0;
+
+	while (*s != '\0')
+	{
+	s++;
+	i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpyu - prints an array of n integers
+ * @src: the string to copy
+ * @dest: the destination buffer
+ * Return: pointer to dest
+ */
+char *_strcpyu(char *dest, char *src)
+{
+	unsigned int i = 0;
+
+	while (src[i] != '\0')
+{
+	dest[i] = src[i];
+	i++;
+	}
+	dest[i] = src[i];
+	return (dest);
 }
 
