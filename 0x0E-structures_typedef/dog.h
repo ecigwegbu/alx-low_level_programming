e3f9e8c7dc74bf99faa564c39ce8f9e3d7762ef7
ecigwegbu <igwegbu@gmail.com>
2022-10-10 12:54:38 +0100

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+typedef struct dog dog;
+
+
+/**
+ * struct dog - dog
+ * @name: the name of the dog
+ * @age: his age
+ * @owner: me
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
