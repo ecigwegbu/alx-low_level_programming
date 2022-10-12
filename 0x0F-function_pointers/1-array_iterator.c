0c0339e46afe457edbbe8256b6028351d165c6d4
ecigwegbu <igwegbu@gmail.com>
2022-10-12 11:42:44 +0100

    0. What's my name

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,9 +6,10 @@
  * @name: the name to print
  * @f: the function to use to print the name
  */
+void array_iterator(int *array, size_t size, void (*action)(int))
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
+	if(f)
 		f(name);
 }
 
