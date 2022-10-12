a8a89bb42bed9a26a005d86a4920011047eb6bd4
ecigwegbu <igwegbu@gmail.com>
2022-10-12 12:21:16 +0100

    1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _ECI_FUNCTION_POINTERS_H
 #define _ECI_FUNCTION_POINTERS_H
 
+int _putchar(char c);
+
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
