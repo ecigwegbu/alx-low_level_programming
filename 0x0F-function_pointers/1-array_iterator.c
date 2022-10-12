d6dda3537b30e298a73bc22d05c05278382728cd
ecigwegbu <igwegbu@gmail.com>
2022-10-12 12:31:59 +0100

    1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	if (!action || !array || !size)
+	if (action == NULL || array == NULL || size == 0)
 		return;
 
 	while (i < size)
