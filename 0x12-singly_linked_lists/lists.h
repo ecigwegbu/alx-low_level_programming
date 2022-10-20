ba514f550355d8c5501fa995c82dc902840ced0c
ecigwegbu <igwegbu@gmail.com>
2022-10-20 23:20:13 +0100

    reqmts update - 0. Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,14 +2,28 @@
 #define _ECI_LINKED_LMAIN_H
 #include <stdlib.h>
 
-typedef struct list_t list_t;
-
-struct list_t
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
 {
 	char *str;
-	int len;
-	list_t *next;
-};
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
+/**
+ * _putchar - print single characters to stdout
+ * @c: the character to print
+ * Return: the character printed
+ */
+int _putchar(char c);
 
 size_t print_list(const list_t *h);
 
