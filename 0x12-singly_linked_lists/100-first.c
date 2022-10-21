2c7b449b7689051ee5a549b5da8f6acfdf330602
ecigwegbu <igwegbu@gmail.com>
2022-10-21 22:28:47 +0100

    5. The Hare and the Tortoise - attribute updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * myConstructor - executes before main
  *
  */
-void myConstructor(void)
+__attribute__ ((constructor)) void myConstructor(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
