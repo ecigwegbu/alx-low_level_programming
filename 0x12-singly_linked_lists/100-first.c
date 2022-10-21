d1a06b9b3f57c8da172c46150b3890590fcba6c9
ecigwegbu <igwegbu@gmail.com>
2022-10-21 22:31:33 +0100

    attribute updated - 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * myConstructor - executes before main
  *
  */
-__attribute__ ((constructor)) void myConstructor(void)
+__attribute__ ((constructor))
+void myConstructor(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
