55ae1c0d8d4e5023ca8b39a53fe5a241c65a8847
ecigwegbu <igwegbu@gmail.com>
2022-10-24 12:13:04 +0100

    2. Add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+/*****************************************************************************/
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a node at the beginning of a simply linked list
+ *
+ * @head: address of the first node pointer
+ * @n: the int value of the new node
+ * Return: the number of nodes printed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp = (NULL);
+
+	/* first check head is not NULL */
+	if (!head)
+		return (NULL);
+
+	temp = malloc(sizeof(listint_t));
+	if (!temp)
+		return (NULL);
+
+	temp->n = n;
+	temp->next = *head;
+	*head = temp; /* de-ref to get node pointer */
+
+	return (*head);
+}
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,8 +12,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -39,15 +39,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
-
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-int freeNode(list_t *node);
+int freeNode(listint_t *node);
 void myConstructor(void) __attribute__ ((constructor));
 
 
