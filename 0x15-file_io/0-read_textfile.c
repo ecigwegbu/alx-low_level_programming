c10ec434771c3556a8305e3c84a38a6d9279f482
ecigwegbu <igwegbu@gmail.com>
2022-10-31 11:20:13 +0100

    0-debug

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,35 +12,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nr = 0, nw;	/* num of characters read/wr in the round */
 	ssize_t count = 0;
 	int fd;
-	char *buff = NULL;
+	char buff[1024];
 
 	if (filename == NULL)
 		return (0);
+
 	if ((letters == 0) || ((ssize_t) letters > SSIZE_MAX))
 		return (0);
+
 	fd = open(filename,  O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buff = malloc(1024);
-	if (!buff)
-		return (0);
+
 	/* read in batches of 1024 characters */
 	while (1)
 	{
 		nr = read(fd, buff, letters - count);
-		if (nr <= 0)	/* ie we have read all we need to read */
+		if (nr <= 0)
 			break;
 		nw = write(STDOUT_FILENO, buff, nr);
-		if (nw != nr)	/* check for write error */
-			return (0);
-
 		count += nw;
 		nr = 0;
 	}
+
 	close(fd);
-	free(buff);
-	/*if (count != (ssize_t) letter) If could not write up to required */
-	/*	return (0); */
+
 	return (count);
 }
 
