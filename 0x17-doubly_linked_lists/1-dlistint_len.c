c383106e5556343fd0a449b872127a655d9edc46
ecigwegbu <igwegbu@gmail.com>
2022-12-09 01:25:38 +0100

    betty checks

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return the length of a doubly linked list
+ * @h: pointer to the head node
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h);
+{
+	const dlistint_t *ptr = h;
+	size_t count = 0;
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+
+	return (count);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -31,9 +31,9 @@ typedef struct stat stat_t;
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 /* team library functions */
@@ -78,6 +78,14 @@ int printEnv(void);
 void freeEnv(char **environ);
 int is_palindrome_num(int num);
 
-size_t print_dlistint(const dlistint_t *h); 
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /*_0x17C_DOUBLE_H*/
