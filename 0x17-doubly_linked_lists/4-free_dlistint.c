31393d765b436ffc409b9231d2e348907068d556
ecigwegbu <igwegbu@gmail.com>
2022-12-09 03:21:55 +0100

    4. Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * free_dlistint- free a doubly linked list
+ * @head: pointer to the head node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr = NULL;
+
+	/* NULL checks */
+	if (head == NULL)
+		return;
+
+	/* scan nodes and free as necessary */
+	if (head == NULL)	/* empty list case */
+		return;
+
+	if (head->next == NULL)		/* single node case */
+	{
+		free(head);
+	}
+	else
+	{
+		ptr = (head)->next;
+		while (ptr->next)
+		{
+			free(head);
+			(head) = ptr;
+			ptr = ptr->next;
+		}
+		free(head);
+		free(ptr);
+	}
+
+	return;
+}
