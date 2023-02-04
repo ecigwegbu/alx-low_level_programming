8fe3302c28d2ade32c12fc9dc1625d326457ada0
ecigwegbu <igwegbu@gmail.com>
2023-02-04 08:22:33 +0100

    1. djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
-
 /**
- * main - this is a placeholder
- * Return: always 0
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
-int main(void)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-	printf("Hello, World!\n");
+	unsigned long int hash;
+	int c;
 
-	return (0);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
-
