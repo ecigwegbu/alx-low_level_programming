e11086c10aa3926bff5b382324be79276dea5a57
ecigwegbu <igwegbu@gmail.com>
2023-02-04 06:14:07 +0100

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,13 +1,31 @@
-#include <stdio.h>
+#include "hash_tables.h"
 
 /**
- * main - this is a placeholder
- * Return: always 0
+ * hash_table_create - create a hash table
+ * @size: the number of elements in the hash table array
+ *
+ * Return: pointer to the hash table structure created (this structure
+ * contains the hash table -ptr to ptr to head node of linked list - and
+ * its size as elements)
  */
-int main(void)
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	printf("Hello, World!\n");
 
-	return (0);
+	hash_table_t *ht;
+
+	/* check size */
+	if (!(size > 0))
+	{
+		return (NULL);
+	}
+
+	/* allocate storage for ht */
+	ht = malloc(size * sizeof(hash_node_t *));
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
+	return (ht);
 }
 
