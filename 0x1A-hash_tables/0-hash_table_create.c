0d47e5811fd88a3ef81de272332cf22762639abc
ecigwegbu <igwegbu@gmail.com>
2023-02-04 08:05:03 +0100

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,26 +11,33 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-	hash_table_t *ht;
+	hash_table_t *ht; /* this struct holds the actual ht & its size*/
+	/* the actual hash table is a hash_node_t* array: */
+	/* hash_table = hash_node_t[] */
 
-	/* check size */
+	/* check size argument */
 	if (!(size > 0))
 	{
 		return (NULL);
 	}
 
-	/* allocate storage for ht */
+	/* allocate storage for ht struct; actual ht array will be all' later*/
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 	{
 		return (NULL);
 	}
 
+
+	/* now initialise the elements of the ht struct */
 	ht->size = size;
 
 	/* allocate array */
 	ht->array = malloc(size * sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		return (NULL);
+	}
 
 	return (ht);
 }
-
