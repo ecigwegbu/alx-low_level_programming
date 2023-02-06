b4198106de9738c54fc06cce5d4d438f226f999d
ecigwegbu <igwegbu@gmail.com>
2023-02-06 05:55:38 +0100

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,13 +1,30 @@
-#include <stdio.h>
+#include "hash_tables.h"
+
+int set_no_collision(hash_table_t *ht, const char *key, const char *value);
+int set_with_collision(hash_table_t *ht, const char *key, const char *value);
 
 /**
- * main - this is a placeholder
- * Return: always 0
+ * hash_table_get - get element in hash table given key
+ * @ht: pointer to the hash table
+ * @key: the unique key for the table lookup
+ *
+ * Return: the associated value if it succeeded, NULL otherwise
  */
-int main(void)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	printf("Hello, World!\n");
+	unsigned long int index;
+	hash_node_t *nptr;
 
-	return (0);
+	if (ht == NULL)
+		return (0);
+
+	index = hash_djb2((const unsigned char *) key) % ht->size;
+	nptr = ht->array[index]; /* temp pointer to element in array / list */
+	for (nptr = ht->array[index]; nptr != NULL; nptr = nptr->next)
+	{
+		if (strcmp(nptr->key, key) == 0)
+			return (nptr->value);
+	}
+
+	return (NULL);  /* not found */
 }
-
