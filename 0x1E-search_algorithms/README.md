73a3f5323a9c99d999b7face2e7e2f09f3f4e2c4
ecigwegbu <igwegbu@gmail.com>
2023-04-12 00:42:37 +0100

    2. Big O #0

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,8 @@
+README.md file for alx-low_level_programming REPO
+0x1E-search_algorithms README
+
+Module: C Programming
+
+This file is for the ALX-SE Training Programme.
+
+At ALX, We DO.HARD.THINGS!
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _ECI_SEARCH_H
+#define _ECI_SEARCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif /* _ECI_SEARCH_H */
+
