3bcd019e17df5d824ec9234c1d67992932dab7c1
ecigwegbu <igwegbu@gmail.com>
2023-04-13 18:42:05 +0100

    11. Advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,7 @@
 #include "search_algos.h"
 
-int advanced_binary_helper(int *array, size_t L, size_t R, int value);
+int advanced_binary_helper(int *array, size_t L, size_t R, int value,
+	size_t *indx);
 void print_array(int *array, size_t size);
 
 /**
@@ -15,11 +16,12 @@ void print_array(int *array, size_t size);
 int advanced_binary(int *array, size_t size, int value)
 {
 	size_t L = 0, R = size - 1;
+	size_t indx = -1; /* pointer to the last found result */
 
 	if (array == NULL || (long int)size < 0)
 		return (-1);
 
-	return (advanced_binary_helper(array, L, R, value));
+	return (advanced_binary_helper(array, L, R, value, &indx));
 }
 
 /**
@@ -29,32 +31,44 @@ int advanced_binary(int *array, size_t size, int value)
  * @L: the index of the first elt of the (sub) array
  * @R: the index of the last elt of the (sub) array
  * @value: the value to search for
+ * @indx: pointer to the last found result
  * Return: the first? index where value is found
  */
-int advanced_binary_helper(int *array, size_t L, size_t R, int value)
+int advanced_binary_helper(int *array, size_t L, size_t R, int value,
+	size_t *indx)
 {
-	int m = 0;
+	size_t m = 0;
+	int mval = 0;
 
-	if (R >= L)
+	if (R > L || ((value > array[R] && R >= L) || ((value < array[L] && R >= L))))
 	{
 		m = L + (R - L) / 2;
+		/* printf("L: %lu, m: %lu, R: %lu\n", L, m, R); */
+		mval = array[m];
 
 		printf("Searching in array: ");
 		print_array(array + L, R - L + 1);
-
-		/* exit condition */
-		if (array[m] == value)
-			return (m);
-
-		/* enter recursion */
-		if (array[m] > value)
-			return (advanced_binary_helper(array, L, m - 1, value));
-		else
-			return (advanced_binary_helper(array, m + 1, R, value));
+		/* exit conditions */
+		if (mval < value)
+		{
+			return (advanced_binary_helper(array, m + 1, R, value, indx));
+		}
+		if (mval > value)
+		{
+			/* return (advanced_binary_helper(array, L, m - 1, value)); */
+			return (advanced_binary_helper(array, L, m - 1, value, indx));
+		}
+		if (mval == value)
+		{
+			*indx = m;
+			/* printf("Indx: %lu\n", *indx);  debug */
+			return (advanced_binary_helper(array, L, m, value, indx));
+		}
 	}
 
+	/* printf("Arrived at end.\n"); */
 	/* value not found */
-	return (-1);
+	return (*indx);
 }
 
 /**
@@ -70,6 +84,3 @@ void print_array(int *array, size_t size)
 	for (i = 0; i < size; i++)
 		printf("%d%s", array[i], i == size - 1 ? "\n" : ", ");
 }
-
-
-
